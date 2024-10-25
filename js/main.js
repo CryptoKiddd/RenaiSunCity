40ebc5763af8f1d033e31cfd36f8bed585946df3
cryptokiddd <avaliani1232@gmail.com>
2024-10-25 17:05:32 +0400

    news,projects and blog pages

--- js/main.js
+++ js/main.js
@@ -1,56 +1,7 @@
 /////////////
 ///MAIN PAGE SLIDER
 ////////////////
-var owl = $('#hero-carousel');
-owl.owlCarousel({
-    loop: true,
-    margin: 10,
-    dots: false, 
-    nav: false, 
-    responsive: {
-        0: { items: 1 },
-    }
-});
 
-// Custom Navigation Buttons
-$('.custom-prev').click(function() {
-    owl.trigger('prev.owl.carousel');
-});
-
-$('.custom-next').click(function() {
-    owl.trigger('next.owl.carousel');
-});
-
-// Custom Dot Click Event
-$('.dot').click(function() {
-    var index = $(this).data('dot');  
-    owl.trigger('to.owl.carousel', [index, 300]); // Navigate to the corresponding slide
-});
-
-// Update Active Dot on Carousel Change
-owl.on('changed.owl.carousel', function(event) {
-    var currentIndex = event.item.index;  // 
-
-    
-    currentIndex = currentIndex - event.relatedTarget._clones.length / 2;
-
-
-    if (currentIndex >= event.item.count) {
-      currentIndex = 0;
-    }
-    if (currentIndex < 0) {
-      currentIndex = event.item.count - 1;
-    }
-
-    // Remove active class from all dots
-    $('.dot').removeClass('active-dot');
-
-    // Add active class to the correct dot based on the currentIndex
-    $('.dot[data-dot="' + currentIndex + '"]').addClass('active-dot');
-});
-
-// Initialize first active dot
-$('.dot[data-dot="0"]').addClass('active-dot');
 
 //set height of hero section to 100vh - navigation height to fit the 100vh screen,  and the same for projects inner page
 const $navigation = $(".header");  
@@ -75,7 +26,12 @@ const projLayoutCol = $('.project-layout-col')
 const projLayoutColWidth = projLayoutCol.outerWidth(true)
 console.log(projLayoutColWidth)
 
-mainPageForm.css("width", `calc(100% - ${projLayoutColWidth}px - 2px )`  )
+if (projLayoutColWidth > 950) {
+  mainPageForm.css("width", `calc(100% - ${projLayoutColWidth}px + 132px)`);
+} else {
+  mainPageForm.css("width", `calc(100% - ${projLayoutColWidth}px - 2px)`);
+}
+
 
 
 
